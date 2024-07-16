608a1e1023eb145caff9a0d6f2b5e6c8383b8f16
Asilkhan Abdumaminov <asilkhan566766@gmail.com>
2024-07-16 12:27:10 +0500

    Initial commit

---
+++ src/main/java/com/example/onlinelessons/OnlinelessonsApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlinelessons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinelessonsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlinelessonsApplication.class, args);
+    }
+
+}
